50e8a4def2cd3b14abcf7e0a181cd137cdebfa49
adg1034 <adg1034@alu.ubu.es>
2026-04-26 22:28:37 +0200

    Actualizar el Servicio para que sea una aplicacion funcional

--- Practica2/gestion-excepciones/src/main/java/com/practica2/gestionexcepciones/service/ApiService.java
+++ Practica2/gestion-excepciones/src/main/java/com/practica2/gestionexcepciones/service/ApiService.java
@@ -1,23 +1,33 @@
 package com.practica2.gestionexcepciones.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class ApiService {
-    private final RestTemplate restTemplate = new RestTemplate();
-    //Apuntar a api de python
-    private final String BASE_URL = "http://localhost:5000/api/error";
 
-    public String testArchivo() {
-        return restTemplate.getForObject(BASE_URL + "/archivo", String.class);
+    private final RestClient restClient = RestClient.create();
+    private final String BASE_URL = "http://localhost:5000/api";
+
+    public String leerArchivo(String nombre) {
+        return restClient.get()
+                .uri(BASE_URL + "/archivo/" + nombre)
+                .retrieve()
+                .body(String.class);
     }
 
-    public String testBaseDatos() {
-        return restTemplate.getForObject(BASE_URL + "/basedatos", String.class);
+    public String consultarBD(String tabla) {
+        // Pasar la tabla a Python
+        return restClient.get()
+                .uri(BASE_URL + "/basedatos/" + tabla)
+                .retrieve()
+                .body(String.class);
     }
 
-    public String testPokemon() {
-        return restTemplate.getForObject(BASE_URL + "/pokemon", String.class);
+    public String buscarPokemon(String nombre) {
+        return restClient.get()
+                .uri(BASE_URL + "/pokemon/" + nombre)
+                .retrieve()
+                .body(String.class);
     }
 }
