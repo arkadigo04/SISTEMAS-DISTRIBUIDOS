f9b737350982c82021e58a66da10565184298968
adg1034 <adg1034@alu.ubu.es>
2026-04-26 19:28:26 +0200

    Configuracion de como Spring Lee Usuarios de bases de datos

---
+++ Practica2/gestion-excepciones/src/main/java/com/practica2/gestionexcepciones/config/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.practica2.gestionexcepciones.config;
+
+import com.practica2.gestionexcepciones.model.User;
+import com.practica2.gestionexcepciones.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
