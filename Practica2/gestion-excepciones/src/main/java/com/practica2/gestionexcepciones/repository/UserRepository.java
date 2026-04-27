ee2f247911f80cd9a8cb8daf45ea7f736386849d
adg1034 <adg1034@alu.ubu.es>
2026-04-27 12:44:23 +0200

    Creacion de la app en python

---
+++ Practica2/gestion-excepciones/src/main/java/com/practica2/gestionexcepciones/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.practica2.gestionexcepciones.repository;
+
+import com.practica2.gestionexcepciones.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
